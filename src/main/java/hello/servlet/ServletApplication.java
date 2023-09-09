3abd847012c09aecbee5216ce844019a869dd5e7
master <ted3131@diquest.com>
2023-09-09 14:38:18 +0900

    commit

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan //서블릿 자동 등록
-@SpringBootApplication
+@SpringBootApplication //스프링 부트 어노테이션
 public class ServletApplication {
 
 	public static void main(String[] args) {
