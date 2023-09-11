330eb13bf66ceeb3e701963bbf956447853c99b5
master <ted3131@diquest.com>
2023-09-11 17:35:12 +0900

    commit

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -6,8 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
-
 
 /*
     1.파라미터 전송기능
