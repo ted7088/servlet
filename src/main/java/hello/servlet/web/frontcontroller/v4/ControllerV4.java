428c4af25ecc6600e760fe0de4234fe8e23f48dc
master <ted3131@diquest.com>
2023-09-21 10:33:22 +0900

    commit

--- src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -2,6 +2,9 @@ package hello.servlet.web.frontcontroller.v4;
 
 import java.util.Map;
 
+//v3와 거의 비슷
+//구현입장에서 modelview를 직접 생성하여 반환하지 않도록 편리한 인터페이스 제공
+
 public interface ControllerV4 {
     /**
      * @param paramMap
--- src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+//어뎁터 도입
+//프레임워크를 유연하고 확장성 있게 설계
+//다형성과 어댑터 덕분에 기존 구조를 유지 및 프레임워크의 기능을 확장
+
 public interface MyHandlerAdapter {
 
     boolean supports(Object handler);
